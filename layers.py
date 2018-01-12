7e45f1d64ac775f28dbc4312eb9e690783de9633
mabirck <mafbirck@inf.ufpel.edu.br>
2018-01-12 09:10:04 -0200

    Adding Base to Standout/Variational Dropout

---
+++ layers.py
@@ -0,0 +1,11 @@
+import torch
+from torch.autograd import Variable
+from torch import nn
+
+Class Standout(nn.Module):
+    def __init__(self, p):
+        self.weight = None
+        self.bias = None
+
+    def forward(self, input):
+        return None
