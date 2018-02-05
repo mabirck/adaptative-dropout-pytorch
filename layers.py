bce7ac120f2fffc6a8719d547109b2abbf66709f
Marco Birck <mafbirck@gmail.com>
2018-02-05 16:49:39 -0200

    Minor Fix

--- layers.py
+++ layers.py
@@ -14,15 +14,14 @@ class Standout(nn.Module):
 
 
     def forward(self, previous, current, p=0.5, deterministic=False):
-        #print(previous.size(), self.pi.size())
         # Function as in page 3 of paper: Variational Dropout
         self.p = self.nonlinearity(self.alpha * previous.matmul(self.pi.t()) + self.beta)
         self.mask = sample_mask(self.p)
-        #print("OYEEEAH", self.mask.data.numpy())
-        if(deterministic or torch.mean(self.p).data.numpy()==0):
+
+        # Deterministic version as in the paper
+        if(deterministic or torch.mean(self.p).data.cpu().numpy()==0):
             return self.p * current
         else:
-            #print(self.mask.size(), current.size(), type(self.mask.data))
             return self.mask * current
 
 def sample_mask(p):
@@ -33,4 +32,10 @@ def sample_mask(p):
     else:
         uniform = Variable(torch.Tensor(p.size()).uniform_(0, 1))
     mask = uniform < p
-    return mask.type(torch.FloatTensor)
+
+    if torch.cuda.is_available():
+        mask.type(torch.cuda.FloatTensor)
+    else:
+        mask.type(torch.FloatTensor)
+
+    return mask
